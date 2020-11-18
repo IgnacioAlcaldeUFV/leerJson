d479bcb76638c595b9f238128ca076795b78baf7
Nacho <7736020@ufv.es>
2020-11-19 00:54:20 +0100

    Se crea la clase persona y se crea el método para extraer los archivos desde json1.json

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,46 @@
 package org.example;
 
+import com.google.gson.Gson;
+import sun.java2d.pipe.BufferedRenderPipe;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args)
     {
-        System.out.println( "Hello World!" );
+        String json = "";
+
+        try {
+            BufferedReader lectorJson = new BufferedReader(new FileReader("json1.json"));
+
+            String linea = "";
+            while( (linea = lectorJson.readLine()) != null ){
+                json += linea;
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(json);
+
+        Gson gson = new Gson();
+        Persona p = gson.fromJson(json, Persona.class);
+
+        p.setEdad(35); // Cambiar edad en el objeto persona
+
+        System.out.println(p);
+
     }
 }
